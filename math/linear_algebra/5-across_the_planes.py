442a5eabfa61e2bfdf99e17062ec17a7a7efed1f
SPCDIAZRIVERACHRISTIAN <christianda3@gmail.com>
2024-12-02 22:22:55 -0400

    fixed condition logic to correctly identify the matrices length match

--- math/linear_algebra/5-across_the_planes.py
+++ math/linear_algebra/5-across_the_planes.py
@@ -12,8 +12,9 @@ def add_matrices2D(mat1, mat2):
         return: a none if they are not the same
         if they are return the sum
     '''
-    if len(mat1) == len(mat2) or len(mat1[0]) != len(mat2[0]):
-        sum = [[mat1[i][j] + mat2[i][j] for j in range(len(mat1[i]))] for i in range(len(mat1))]
-        return sum
-    else:
+    if len(mat1) != len(mat2) or len(mat1[0]) != len(mat2[0]):
         return None
+    else:
+        sum = [[mat1[i][j] + mat2[i][j] for j in range(len(mat1[i]))]
+               for i in range(len(mat1))]
+        return sum
