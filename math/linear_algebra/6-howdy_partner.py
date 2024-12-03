4fa65553ab82bba345805c44edb6da1fc51ddc13
SPCDIAZRIVERACHRISTIAN <christianda3@gmail.com>
2024-12-02 22:30:38 -0400

    Added a concatenation function file

---
+++ math/linear_algebra/6-howdy_partner.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+'''this function concatenates two arrays'''
+
+
+def cat_arrays(arr1, arr2):
+    '''concatenates and returns a concatenated array
+
+    Args:
+        arr1 (list): an integer array
+        arr2 (list): An integer array
+
+        return: a new list
+    '''
+    return arr1 + arr2
