e494ff82555b37c02e211aa0fb4deef20121ec4d
SPCDIAZRIVERACHRISTIAN <christianda3@gmail.com>
2024-12-02 22:59:53 -0400

    Add docstring to cat_matrices2D function for better clarity

--- math/linear_algebra/7-gettin_cozy.py
+++ math/linear_algebra/7-gettin_cozy.py
@@ -3,7 +3,19 @@
 
 
 def cat_matrices2D(mat1, mat2, axis=0):
+    '''This returns concatenated matrices
+    by their axis
 
+    Args:
+        mat1 (list): _description_
+        mat2 (list): _description_
+        axis (int, optional): specifying if it
+        will be concatenated by rows(0) or columns(1).
+        Defaults to 0.
+
+    Returns:
+        list: concatenated matrices
+    '''
     mat_cp1 = [row[:] for row in mat1]
     mat_cp2 = [row[:] for row in mat2]
     if axis == 0:
