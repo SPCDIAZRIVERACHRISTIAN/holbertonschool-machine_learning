bcfcb006ce64b59fec92bc34b56dc6d409468c52
Christian <christianda3@gmail.com>
2024-12-02 22:11:54 -0400

    Add function to add two 2D matrices with validation for dimensions

---
+++ math/linear_algebra/5-across_the_planes.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+'''This function adds two matrices if they are the same'''
+
+
+def add_matrices2D(mat1, mat2):
+    '''_summary_
+
+    Args:
+        mat1 (list): a 2d matrix
+        mat2 (list): a 2d matrix
+        sum (list): sum of mat1 and mat2
+        return: a none if they are not the same
+        if they are return the sum
+    '''
+    if len(mat1) == len(mat2) or len(mat1[0]) != len(mat2[0]):
+        sum = [[mat1[i][j] + mat2[i][j] for j in range(len(mat1[i]))] for i in range(len(mat1))]
+        return sum
+    else:
+        return None
