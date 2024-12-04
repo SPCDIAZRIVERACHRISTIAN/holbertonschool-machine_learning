37f33328d06e9f88ddff26441cea86db769a57d3
Christian <christianda3@gmail.com>
2024-12-04 15:00:01 -0400

    revert changes

--- math/linear_algebra/100-slice_like_a_ninja.py
+++ math/linear_algebra/100-slice_like_a_ninja.py
@@ -6,20 +6,15 @@ def np_slice(matrix, axes={}):
     '''this function returns a new numpy.ndarray of sliced matrix'''
     # find the number of axis
     num_axes = matrix.ndim
-    matrix_new = matrix.copy()
-    print(f"dimentions {num_axes}")
 
     # slice
     slices = [slice(None)] * num_axes
-    print(f"slice {slices}")
 
     # update
     for axis, slice_range in axes.items():
-        print(f"axis {axis}")
-        print(f"slice range {slice_range}")
         slices[axis] = slice(*slice_range)
 
 
-    return matrix_new[tuple(slices)]
+    return matrix[tuple(slices)]
 
 
