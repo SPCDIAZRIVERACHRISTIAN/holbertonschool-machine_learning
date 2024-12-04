b7f069285fa9d871d5f2e38bee957f7c2e41b9d0
Christian <christianda3@gmail.com>
2024-12-04 11:17:20 -0400

    Refactor np_transpose function to use method chaining for matrix transposition

--- math/linear_algebra/11-the_western_exchange.py
+++ math/linear_algebra/11-the_western_exchange.py
@@ -1,8 +1,7 @@
 #!/usr/bin/env python3
 '''this function returns a transposed matrix'''
 
-import numpy as np
 
 
 def np_transpose(matrix):
-    return np.transpose(matrix)
+    return matrix.transpose()
