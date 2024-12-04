8a1f17e3af4fec3de8b6e4ebc60e250f3a90aace
Christian <christianda3@gmail.com>
2024-12-04 10:46:01 -0400

    Add numpy shape and transpose functions with examples

---
+++ math/linear_algebra/10-ill_use_my_scale.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+'''this function calculates the shape of a numpy array'''
+
+
+def np_shape(matrix):
+    '''Return shape of numpy'''
+    return matrix.shape
---
+++ math/linear_algebra/11-the_western_exchange.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+'''this function returns a transposed matrix'''
+
+import numpy as np
+
+
+def np_transpose(matrix):
+    return np.transpose(matrix)
