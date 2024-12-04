79fd0ab5c0ec92d76fc2cd2a69c99ab41394af46
Christian <christianda3@gmail.com>
2024-12-04 14:56:39 -0400

    Add np_slice function for slicing matrices along specified axes

---
+++ math/linear_algebra/100-slice_like_a_ninja.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+'''this function slices a matrix along specific axes'''
+
+
+def np_slice(matrix, axes={}):
+    '''this function returns a new numpy.ndarray of sliced matrix'''
+    # find the number of axis
+    num_axes = matrix.ndim
+    print(f"dimentions {num_axes}")
+
+    # slice
+    slices = [slice(None)] * num_axes
+    print(f"slice {slices}")
+
+    # update
+    for axis, slice_range in axes.items():
+        print(f"axis {axis}")
+        print(f"slice range {slice_range}")
+        slices[axis] = slice(*slice_range)
+
+
+    return matrix[tuple(slices)]
+
+
