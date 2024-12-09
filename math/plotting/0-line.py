903ae0bdd6f9a09a8263cbf1ad94ca20a60c7615
Christian <christianda3@gmail.com>
2024-12-09 14:19:03 -0400

    Added documentastion and fixed pycodestyle

--- math/plotting/0-line.py
+++ math/plotting/0-line.py
@@ -1,8 +1,11 @@
 #!/usr/bin/env python3
+'''This function shows a plotted line graph '''
 import numpy as np
 import matplotlib.pyplot as plt
 
+
 def line():
+    '''shows a plotted linear graph'''
 
     y = np.arange(0, 11) ** 3
     plt.figure(figsize=(6.4, 4.8))
