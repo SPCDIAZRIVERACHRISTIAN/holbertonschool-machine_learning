8df5707fcc112a079b82594c112b6ef1ec3bbccf
Christian <christianda3@gmail.com>
2024-12-10 14:14:48 -0400

    Update histogram bin edges to include maximum grade

--- math/plotting/4-frequency.py
+++ math/plotting/4-frequency.py
@@ -13,7 +13,7 @@ def frequency():
     plt.figure(figsize=(6.4, 4.8))
     plt.xlabel('Grades')
     plt.ylabel('Number of Students')
-    bin_edges = np.arange(0, 100, 10)
+    bin_edges = np.arange(0, 110, 10)
     plt.hist(student_grades, bins=bin_edges, edgecolor='black')
     plt.title("Project A")
     plt.show()
