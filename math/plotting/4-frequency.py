f9370b2d24d2b0777db91afd3856fc5f8df488b9
Christian <christianda3@gmail.com>
2024-12-10 14:17:38 -0400

    Refactor histogram plotting to simplify bin edges and improve clarity

--- math/plotting/4-frequency.py
+++ math/plotting/4-frequency.py
@@ -4,16 +4,12 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-
 def frequency():
     '''This function shows a histogram'''
-
     np.random.seed(5)
     student_grades = np.random.normal(68, 15, 50)
-    plt.figure(figsize=(6.4, 4.8))
+    plt.hist(student_grades, bins=range(0, 101, 10), edgecolor='black')
     plt.xlabel('Grades')
     plt.ylabel('Number of Students')
-    bin_edges = np.arange(0, 110, 10)
-    plt.hist(student_grades, bins=bin_edges, edgecolor='black')
-    plt.title("Project A")
+    plt.title('Project A')
     plt.show()
