9fbe9cf2b1d68f7e1b4016e189be647e83fbc8fe
Christian <christianda3@gmail.com>
2024-12-10 14:22:17 -0400

    Enhance histogram visualization by adjusting figure size, setting x and y limits, and adding x-ticks for clarity

--- math/plotting/4-frequency.py
+++ math/plotting/4-frequency.py
@@ -8,8 +8,12 @@ def frequency():
     '''This function shows a histogram'''
     np.random.seed(5)
     student_grades = np.random.normal(68, 15, 50)
-    plt.hist(student_grades, bins=range(0, 101, 10), edgecolor='black')
+    plt.figure(fgsize=(6.4, 4.8))
     plt.xlabel('Grades')
     plt.ylabel('Number of Students')
     plt.title('Project A')
+    plt.hist(student_grades, bins=range(0, 101, 10), edgecolor='black')
+    plt.xticks(range(0, 101, 10))
+    plt.xlim(0, 100)
+    plt.ylim(0, 30)
     plt.show()
