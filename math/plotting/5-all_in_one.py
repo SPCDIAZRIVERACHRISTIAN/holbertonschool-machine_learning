b9f2a15f6043ab7643081d20f64cbef75d54a9aa
SPCDIAZRIVERACHRISTIAN <christianda3@gmail.com>
2024-12-10 20:26:50 -0400

    Added file tasked to plot 5 graphs

--- math/plotting/5-all_in_one.py
+++ math/plotting/5-all_in_one.py
@@ -3,6 +3,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+
 def all_in_one():
     '''This function plots 5 graphs'''
 
@@ -29,4 +30,51 @@ def all_in_one():
     np.random.seed(5)
     student_grades = np.random.normal(68, 15, 50)
 
+    fig = plt.figure(figsize=(5, 5))
+    fig.suptitle('All in One')
 
+    plt.subplot(3, 2, 1)
+    plt.plot(y0, 'r')
+    plt.xticks(range(0, 11, 2))
+    plt.yticks(range(0, 1100, 500))
+    plt.xlim(0, 10)
+
+    plt.subplot(322)
+    plt.scatter(x1, y1, c='magenta')
+    plt.xlabel('Height (in)', fontsize='x-small')
+    plt.ylabel('weight (lbs)', fontsize='x-small')
+    plt.title('men\'s HEight vs Weight', fontsize='x-small')
+
+    plt.subplot(3, 2, 3)
+    plt.plot(x2, y2)
+    plt.yscale('log')
+    plt.xticks(range(0, 30000, 10000))
+    plt.xlim(0, 28650)
+    plt.xlabel('Time (years)', fontsize='x-small')
+    plt.ylabel('Fraction Remaining', fontsize='x-small')
+    plt.title('Exponential Decay of C-14', fontsize='x-small')
+
+    plt.subplot(324)
+    plt.plot(x3, y31, 'r--', label='C-14')
+    plt.plot(x3, y32, 'g-', label='Ra-226')
+    plt.xticks(np.arange(0, 21000, step=5000))
+    plt.xlabel('Time (years)', fontsize='x-small')
+    plt.xlim(0, 20000)
+    plt.yticks(np.arange(0, 1.2, step=0.5))
+    plt.ylabel('Fraction Remaining', fontsize='x-small')
+    plt.ylim(0, 1)
+    plt.title('Exponential Decay of Radioactive Elements', fontsize='x-small')
+    plt.legend(fontsize='x-small')
+
+    plt.subplot(3, 2, (5, 6))
+    plt.hist(student_grades, bins=range(0, 101, 10), edgecolor='black')
+    plt.xticks(range(0, 101, 10))
+    plt.yticks(range(0, 31, 10))
+    plt.xlabel('Grades', fontsize='x-small')
+    plt.ylabel('Number of Students', fontsize='x-small')
+    plt.title('Project A', fontsize='x-small')
+    plt.xlim(0, 100)
+    plt.ylim(0, 30)
+
+    plt.tight_layout()
+    plt.show()
