d5c6045a25f07c14a93cabc08cab282ba153d823
SPCDIAZRIVERACHRISTIAN <christianda3@gmail.com>
2024-12-12 10:36:05 -0400

    Add 3D PCA scatter plot visualization for Iris dataset

--- math/plotting/101-pca.py
+++ math/plotting/101-pca.py
@@ -15,3 +15,22 @@ norm_data = data - data_means
 _, _, Vh = np.linalg.svd(norm_data)
 pca_data = np.matmul(norm_data, Vh[:3].T)
 
+# Create a new figure
+fig = plt.figure()
+
+# Add a 3D subplot
+ax = fig.add_subplot(111, projection='3d')
+
+# Scatter plot, with color determined by the labels
+scatter = ax.scatter(pca_data[:, 0], pca_data[:, 1], pca_data[:, 2], c=labels, cmap='plasma')
+
+# Set the labels for the x, y, and z axes
+ax.set_xlabel('U1')
+ax.set_ylabel('U2')
+ax.set_zlabel('U3')
+
+# Set the title of the plot
+plt.title('PCA of Iris Dataset')
+
+# Display the plot
+plt.show()
