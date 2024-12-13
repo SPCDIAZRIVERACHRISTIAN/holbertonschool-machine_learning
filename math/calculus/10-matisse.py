4576bc5cdd54a77490ca8ae3ef393e3d0d407c11
SPCDIAZRIVERACHRISTIAN <christianda3@gmail.com>
2024-12-13 11:16:56 -0400

    Add docstring to poly_derivative function for clarity on usage and return value

--- math/calculus/10-matisse.py
+++ math/calculus/10-matisse.py
@@ -3,7 +3,16 @@
 
 
 def poly_derivative(poly):
-    ''''''
+    '''calculates the derivative of a polynomial
+
+    Args:
+        poly (list): poly is a list of coefficients
+        representing a polynomial
+
+    Returns:
+        list: Return a new list of coefficients representing
+        the derivative of the polynomial
+    '''
     if not isinstance(poly, list) or len(
             poly) == 0 or not all(isinstance(c, (int, float)) for c in poly):
         return None
