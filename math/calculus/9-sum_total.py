0891575a2f565330033cae64d3996f045665f23c
SPCDIAZRIVERACHRISTIAN <christianda3@gmail.com>
2024-12-13 10:47:23 -0400

    Add summation function for sigma notation in 9-sum_total.py

---
+++ math/calculus/9-sum_total.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+'''this function does a sigma notation and returns the result'''
+
+def summation_i_squared(n):
+    '''returns the sum of the range of n'''
+    # derive the sigma notation and return it
+    return n * (n + 1) * (2 * n + 1) // 6
