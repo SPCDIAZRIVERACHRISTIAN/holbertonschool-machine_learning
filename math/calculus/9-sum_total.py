687cd2ff021a7dddf9665126c1feaead83919fe1
Christian <christianda3@gmail.com>
2024-12-14 20:44:46 -0400

    Add input validation to summation_i_squared function to ensure n is an integer

--- math/calculus/9-sum_total.py
+++ math/calculus/9-sum_total.py
@@ -4,8 +4,8 @@
 
 def summation_i_squared(n):
     '''returns the sum of the range of n'''
-    if isinstance(n, int):
+    if not isinstance(n, int):
+        return None
+    else:
         # derive the sigma notation and return it
         return n * (n + 1) * (2 * n + 1) // 6
-    else:
-        return None
