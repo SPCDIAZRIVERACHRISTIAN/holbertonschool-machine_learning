f17deed4cdf168ac72b08578c44603adf7c19212
Christian <christianda3@gmail.com>
2024-12-14 20:42:57 -0400

    Added condition to return none

--- math/calculus/9-sum_total.py
+++ math/calculus/9-sum_total.py
@@ -7,3 +7,5 @@ def summation_i_squared(n):
     if isinstance(n, int):
         # derive the sigma notation and return it
         return n * (n + 1) * (2 * n + 1) // 6
+    else:
+        return None
