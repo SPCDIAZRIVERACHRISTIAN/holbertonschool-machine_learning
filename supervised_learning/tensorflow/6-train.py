00f997485311ed331581b52dc5a3b2eb8f36b096
SPCDIAZRIVERACHRISTIAN <christianda3@gmail.com>
2025-01-14 21:02:07 -0400

    Remove unused numpy import from the neural network training script

--- supervised_learning/tensorflow/6-train.py
+++ supervised_learning/tensorflow/6-train.py
@@ -2,7 +2,6 @@
 '''This is a function that builds,
     trains, and saves a neural network classifier'''
 
-import numpy as np
 import tensorflow.compat.v1 as tf  # type: ignore
 calculate_accuracy = __import__('3-calculate_accuracy').calculate_accuracy
 calculate_loss = __import__('4-calculate_loss').calculate_loss
