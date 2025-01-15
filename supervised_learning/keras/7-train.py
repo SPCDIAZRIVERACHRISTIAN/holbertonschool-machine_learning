d8753f625195d06a737c4722f0bae11efd861fd1
SPCDIAZRIVERACHRISTIAN <christianda3@gmail.com>
2025-01-15 19:41:46 -0400

    Refactor train_model function to include learning rate decay and improve docstring clarity

--- supervised_learning/keras/7-train.py
+++ supervised_learning/keras/7-train.py
@@ -1,53 +1,82 @@
 #!/usr/bin/env python3
-'''This function trains a model using mini-batch gradient descent'''
+"""This module implements learning rate decay
+"""
+import tensorflow.keras as K
 
-import tensorflow.keras as K  # type:ignore
 
+def train_model(
+    network,
+    data,
+    labels,
+    batch_size,
+    epochs,
+    validation_data=None,
+    early_stopping=False,
+    patience=0,
+    learning_rate_decay=False,
+    alpha=0.1,
+    decay_rate=1,
+    verbose=True,
+    shuffle=False,
+):
+    """
+    Train a model using mini-batch gradient descent and
+    also analyze validation data.
 
-def train_model(network,
-                data,
-                labels,
-                batch_size,
-                epochs,
-                validation_data=None,
-                early_stopping=False,
-                patience=0,
-                learning_rate_decay=False,
-                alpha=0.1,
-                decay_rate=1,
-                verbose=True,
-                shuffle=False
-                ):
-    '''trains a model using mini-batch gradient descent
-
-    Args:
-        network (model): is the model to train
-        data (ndarray):  containing the input data
-        labels (ndarray): containing the labels of data
-        batch_size (int): size of the batch used for
-            mini-batch gradient descent
-        epochs (int): number of passes through data
-            for mini-batch gradient descen
-        verbose (bool, optional): determines if
-            output should be printed during training. Defaults to True.
-        shuffle (bool, optional): determines whether
-            to shuffle the batches every epoch. Normally,
-            it is a good idea to shuffle,
-            but for reproducibility. Defaults to False.
+    Parameters:
+    network (keras model): The model to train.
+    data (numpy.ndarray): The input data, of shape (m, nx).
+    labels (numpy.ndarray): The labels of the data,
+    one-hot encoded, of shape (m, classes).
+    batch_size (int): The size of the batch used for
+    mini-batch gradient descent.
+    epochs (int): The number of passes through data for
+    mini-batch gradient descent.
+    validation_data (tuple): Data to validate the model with, if not None.
+    early_stopping (bool): Whether to use early stopping mechanism.
+    patience (int): Number of epochs to wait for improvement before stopping.
+    learning_rate_decay (bool): Whether to use learning rate decay.
+    alpha (float): Initial learning rate.
+    decay_rate (float): The decay rate.
+    verbose (bool, optional): Determines if output should
+    be printed during training. Default is True.
+    shuffle (bool, optional): Determines whether to shuffle
+    the batches every epoch. Default is False.
 
     Returns:
-        History object: a record of training loss values and metrics values at
-            successive epochs, as well as validation loss values and validation
-            metrics values (if applicable).
-    '''
-    early_stopping = K.callbacks.EarlyStopping(monitor='val_loss', patience=patience, restore_best_weights=True)
-    history = network.fit(data,
-                          labels,
-                          batch_size=batch_size,
-                          epochs=epochs,
-                          verbose=verbose,
-                          shuffle=shuffle,
-                          validation_data=validation_data,
-                          callbacks=[early_stopping]
-                          )
+    history: The History object generated after training the model.
+    """
+
+    callbacks = []
+
+    # If early_stopping is True and validation_data is provided,
+    # use EarlyStopping callback
+    if early_stopping and validation_data:
+        callbacks.append(
+            K.callbacks.EarlyStopping(monitor="val_loss", patience=patience)
+        )
+
+    # If learning_rate_decay is True and validation_data is provided,
+    # use LearningRateScheduler callback
+    if learning_rate_decay and validation_data:
+
+        def scheduler(epoch):
+            return alpha / (1 + decay_rate * epoch)
+
+        callbacks.append(K.callbacks.LearningRateScheduler(scheduler,
+                                                           verbose=1))
+
+    # Fit the model using the provided parameters. If validation_data
+    # is not None, it will be used for validation.
+    history = network.fit(
+        data,
+        labels,
+        batch_size=batch_size,
+        epochs=epochs,
+        verbose=verbose,
+        shuffle=shuffle,
+        validation_data=validation_data,
+        callbacks=callbacks,
+    )
+
     return history
