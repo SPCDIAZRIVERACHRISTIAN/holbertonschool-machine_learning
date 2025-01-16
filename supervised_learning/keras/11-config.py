b40d910204f923cae11f61be46db01757cadeeec
SPCDIAZRIVERACHRISTIAN <christianda3@gmail.com>
2025-01-16 19:12:13 -0400

    Add functions to save/load model configuration and test model performance

---
+++ supervised_learning/keras/11-config.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+'''This functions save and load configuration in json'''
+
+import tensorflow.keras as K
+
+
+def save_config(network, filename):
+    '''saves a model’s configuration in JSON format
+
+    Args:
+        network: is the model whose configuration should be saved
+        filename: is the path of the file that the configuration should be saved to
+    '''
+    json_config = network.to_json()
+    with open(filename, 'w') as json_file:
+        json_file.write(json_config)
+
+
+def load_config(filename):
+    '''loads a model with a specific configuration
+
+    Args:
+        filename: is the path of the file containing the
+            model’s configuration in JSON format
+    '''
+    with open(filename, 'r') as json_file:
+        json_config = json_file.read()
+    return K.models.model_from_json(json_config)
---
+++ supervised_learning/keras/12-test.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+'''This function tests the model'''
+
+import tensorflow.keras as K  # type: ignore
+
+
+def test_model(network, data, labels, verbose=True):
+    '''test models
+
+    Args:
+        network: is the network model to test
+        data: is the input data to test the model with
+        labels: are the correct one-hot labels of data
+        verbose: is a boolean that determines if output should be
+            printed during the testing process
+    '''
+    return network.evaluate(data, labels, verbose=verbose)
