01cbd2b3c2d2a7014b1c477109f52b8e0ed71703
SPCDIAZRIVERACHRISTIAN <christianda3@gmail.com>
2025-01-22 15:06:14 -0400

    Add momentum optimization function for SGD in TensorFlow

---
+++ supervised_learning/optimization/6-momentum.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+'''
+NOTE
+the keras library has a module for optimizing
+momentum the way to access it is
+tensorflow.keras.optimizers.
+in this case we are using SGD
+which stands for Stochastic
+Gradient Descent.
+'''
+
+import tensorflow as tf
+
+
+def create_momentum_op(alpha, beta1):
+    '''
+    Set up the gradient descent with momentum
+    optimization algorithm in TensorFlow.
+
+    Parameters:
+    alpha (float): The learning rate.
+    beta1 (float): The momentum weight.
+
+    Returns:
+    tf.keras.optimizers.SGD: The SGD optimizer object with momentum.
+    '''
+    return tf.keras.optimizers.SGD(learning_rate=alpha, momentum=beta1)
