9c0c78a6e8763deb527d5284652a93dbf3ab0c31
SPCDIAZRIVERACHRISTIAN <christianda3@gmail.com>
2025-02-12 20:37:03 -0400

    Fix string quotes in inception_network.py for consistency

--- supervised_learning/deep_cnns/1-inception_network.py
+++ supervised_learning/deep_cnns/1-inception_network.py
@@ -4,7 +4,7 @@ described in Going Deeper with Convolutions (2014):
 """
 from tensorflow import keras as K
 
-inception_block = __import__("0-inception_block").inception_block
+inception_block = __import__('0-inception_block').inception_block
 
 
 def inception_network():
